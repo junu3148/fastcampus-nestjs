dba4c9ea02d5bc2b0dac179d36115980a652a9eb
junu <menstua@viking-lab.com>
2024-04-18 11:29:38 +0900

    Feat 데이터베이스 연결

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,10 @@
 import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
 
-export default () => ConfigModule.forRoot({
+export default () =>
+  ConfigModule.forRoot({
     isGlobal: true,
-    envFilePath: `.env.${process.env.NODE_ENV}`,
-    load: [configuration]
-});
+    envFilePath: `.env.local`,
+    load: [configuration],
+  });
+
